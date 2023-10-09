971e4013e10e2e1df4e1a256cd8c4c40acd83e55
MrezaVasebi <reza.vaseb@gmail.com>
2023-10-09 18:12:41 +0330

    added: icons were added

--- modules/handleIcons.tsx
+++ modules/handleIcons.tsx
@@ -1,6 +1,22 @@
 export function handleIcons(iconName: string) {
   let iconList = {
     logo: require("../assets/icons/logo.png"),
+    bag: require("../assets/icons/bag.png"),
+    cart: require("../assets/icons/cart.png"),
+    down: require("../assets/icons/down.png"),
+    edit: require("../assets/icons/edit.png"),
+    home: require("../assets/icons/home.png"),
+    income: require("../assets/icons/income.png"),
+    info: require("../assets/icons/info.png"),
+    like: require("../assets/icons/like.png"),
+    list: require("../assets/icons/list.png"),
+    mail: require("../assets/icons/mail.png"),
+    notif: require("../assets/icons/notif.png"),
+    right: require("../assets/icons/right.png"),
+    search: require("../assets/icons/search.png"),
+    setting: require("../assets/icons/setting.png"),
+    start: require("../assets/icons/start.png"),
+    user: require("../assets/icons/user.png"),
   };
 
   return iconList[iconName as keyof typeof iconList];
